43c1604fc16e7e3cfdc5706797622da164b058fb
IrynaZink <irynazinkovych@gmail.com>
2026-03-14 22:35:21 +0100

    hw13.03

--- src/test/java/com/ait/test/DemoWebShopFind.java
+++ src/test/java/com/ait/test/DemoWebShopFind.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class DemoWebShopFindLocators {
+public class DemoWebShopFind {
     WebDriver driver;
     @BeforeMethod
     public void setUp(){
